0d4ace9a441d22addc107daf8bcf6aec74e37343
anshshr <anshshrivastav24@gmail.com>
2025-07-17 16:04:32 +0530

    added the cors package to avoid cors issue

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,12 +1,14 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import sweetRoute from './routes/sweetRoutes'
+import cors from "cors";
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
 
-app.use("/api/sweets" ,sweetRoute)
+app.use("/api/sweets", sweetRoute)
 
 
 app.listen(PORT, () => {
